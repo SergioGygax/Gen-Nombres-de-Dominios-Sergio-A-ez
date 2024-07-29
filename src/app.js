cb5ad1b93d39c74eba5d6257ac9493da6bad9165
Sergio Añez <160168084+SergioGygax@users.noreply.github.com>
2024-07-29 18:51:09 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -1,26 +1,24 @@
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let domain = [".com", ".net", ".es", ".org"];
-const domainContainer = document.querySelector(".domain-list");
+import "bootstrap";
+import "./style.css";
 
-function getRandomDomain() {
-  let randomDomainsArr = [];
-  for (let pronouns of pronoun) {
-    let randomDomain = "";
-    for (let adjs of adj) {
-      for (let nouns of noun) {
-        for (let domains of domain) {
-          randomDomain = `${pronouns}${adjs}${nouns}${domains}`;
-          if (randomDomainsArr.length === 0) {
-            randomDomainsArr.push(randomDomain);
-          } else randomDomainsArr.push("<br>" + randomDomain);
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  //write your code here
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".es", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + extension[l]);
         }
       }
     }
   }
-  return randomDomainsArr;
-}
-domainContainer.innerHTML = getRandomDomain();
-
-console.log(getRandomDomain());
+};
